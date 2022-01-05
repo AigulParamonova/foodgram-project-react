fc4b9564566d22d7dab2b4220064e9b1b2280075
AigulParamonova <aigul.paramonova@yandex.ru>
2022-01-05 16:35:59 +0300

    Change23

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -136,4 +136,3 @@ AUTH_USER_MODEL = 'users.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
-MAIL = '<admin@foodgram.ru>'
