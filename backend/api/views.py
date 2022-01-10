c6a08278f143d44a3b893af150de3d9b7734ee71
AigulParamonova <aigul.paramonova@yandex.ru>
2022-01-10 19:24:45 +0300

    test2

--- backend/api/views.py
+++ backend/api/views.py
@@ -96,8 +96,8 @@ class CustomUserViewSet(UserViewSet):
             return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
         Subscribe.objects.filter(user=user, author=author).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-        
-        
+
+
 class TagsViewSet(ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
