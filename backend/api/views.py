1c7e96dfdca172c8662946a369f00ae1f0b5d31b
AigulParamonova <aigul.paramonova@yandex.ru>
2022-01-12 19:44:46 +0300

    change subscriptions

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
